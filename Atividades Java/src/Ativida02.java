dfdff5ad58e51b87d68b400b7542b32ed5cbfe9e
Gabriela Miranda <Gabriela48938486@edu.df.senac.br>
2026-07-13 20:31:15 +0000

    Adicionando Atividade02

---
+++ Atividades Java/src/Ativida02.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ativida02 {
+
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.print("Digite sua idade: ");
+
+        
+        int idade = scanner.nextInt();
+
+        
+        if (idade < 16) {
+
+            System.out.println("Não pode votar.");
+
+        }
+        
+        else if (idade == 16 || idade == 17) {
+
+            System.out.println("Voto não obrigatorio.");
+
+        }
+        
+        else if (idade <= 70) {
+
+            System.out.println("Voto obrigatório.");
+
+        }
+        
+        else {
+
+            System.out.println("Voto facultativo.");
+
+        }
+    }
+}
