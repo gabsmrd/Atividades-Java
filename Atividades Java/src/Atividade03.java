d37a5e815b641503817c17103aef08710bf5c581
Gabriela Miranda <Gabriela48938486@edu.df.senac.br>
2026-07-13 20:32:08 +0000

    Adicionando Atividade03

---
+++ Atividades Java/src/Atividade03.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Atividade03 {
+
+    public static void main(String[] args) {
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        
+        System.out.println("Média: " + media);
+
+        
+        if (media >= 7.0) {
+
+            System.out.println("Aprovado.");
+
+        } else if (media >= 5.0) {
+
+            System.out.println("Em Recuperação.");
+
+        } else {
+
+            System.out.println("Reprovado.");
+
+        }
+
+    }
+
+}
