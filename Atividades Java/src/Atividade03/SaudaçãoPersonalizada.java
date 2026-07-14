2f91a96169fcc6f401696edb1f0f53aa4165d534
Gabriela Miranda <Gabriela48938486@edu.df.senac.br>
2026-07-14 17:27:11 +0000

    Enviando exercicio SaudaçãoPersonalizada atividade03

---
+++ Atividades Java/src/Atividade03/SaudaçãoPersonalizada.java
@@ -0,0 +1,17 @@
+package Atividade03;
+import java.util.Scanner;
+
+public class SaudaçãoPersonalizada {
+    public static void imprimirSaudacao(String nome) {
+        System.out.print("Olá, " + nome + " Seja bem vindo a nossa aula de de métodos!\n ");
+    }
+
+
+public static void main(String[] args){
+    Scanner inicioNome = new Scanner(System.in);
+
+    System.out.print("Digite seu nome: ");
+    String nome = inicioNome.nextLine();
+    imprimirSaudacao(nome);
+    }
+}
