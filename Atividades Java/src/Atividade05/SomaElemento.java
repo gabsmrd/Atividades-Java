875a780e40c3efa9df9a24f2cc4883d8c45342a1
Gabriela Miranda <Gabriela48938486@edu.df.senac.br>
2026-07-16 18:35:48 +0000

    Atualizando exercicio SomaElemnto atividade05

--- Atividades Java/src/Atividade05/SomaElemento.java
+++ Atividades Java/src/Atividade05/SomaElemento.java
@@ -8,24 +8,23 @@ public class SomaElemento {
         System.out.println("Digite 5 números inteiros: ");
 
         for (int inicio = 0; inicio < numeros.length; inicio++) {
-            System.out.print("Digite o " + (inicio + "primeiro ") + "número: ");
+            System.out.print("Digite o " + (inicio + "1") + "° número: ");
             numeros[inicio] = entradaNumeros.nextInt();
         }
 
         System.out.print("Vetor preenchido: [");
 
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.print(numeros[i]);
-            if (i < numeros.length - 1) {
+        for (int inicio2 = 0; inicio2 < numeros.length; inicio2++) {
+            System.out.print(numeros[inicio2]);
+            if (inicio2 < numeros.length - 1) {
                 System.out.print(", ");
             }
         }
 
         System.out.println("]");
         int soma = 0;
-
-        for (int inicio2 = 0; inicio2 < numeros.length; inicio2++) {
-            soma += numeros[inicio2];
+        for (int inicio3 = 0; inicio3 < numeros.length; inicio3++) {
+            soma += numeros[inicio3];
         }
         System.out.println("A soma de todos os elementos é: " + soma);
     }
