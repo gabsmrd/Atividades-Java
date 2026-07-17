1b2563381fedcdf123709551413715fb131d1851
Gabriela Miranda <Gabriela48938486@edu.df.senac.br>
2026-07-16 21:19:52 -0300

    Enviando exercicio MelhoriaTarefas Atividade06

---
+++ Atividades Java/src/Atividade06/MelhoriaTarefas.java
@@ -0,0 +1,11 @@
+package Atividade06;
+
+//Ordenação de tarefas em ordem alfabetica/tadas
+//Metodo para remover tarefas espefícicas
+//> removerTarefas pelo metodo FIFO
+//> removerTarefas pelo metodo FEFO
+//> removerTarefas pelo metodo LIFO
+//(Opicional) Adicionar um metodo que registre as tarefas que foram concluídas
+
+public class MelhoriaTarefas {
+}
