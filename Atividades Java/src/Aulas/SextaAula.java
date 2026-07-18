88ed32e29fcdf5ac21f51c2238cf2a70cf6e7746
Gabriela Miranda <Gabriela48938486@edu.df.senac.br>
2026-07-17 21:46:39 -0300

    Envidando SextaAula

---
+++ Atividades Java/src/Aulas/SextaAula.java
@@ -0,0 +1,12 @@
+package Aulas;
+import Aulas.Objeto.Pessoa;
+
+public class SextaAula {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+
+
+        System.out.println(pessoa1);
+
+    }
+}
--- Atividades Java/src/Aulas/objeto/Pessoa.java
+++
@@ -1,4 +0,0 @@
-package Aulas.objeto;
-
-public class Pessoa {
-}
